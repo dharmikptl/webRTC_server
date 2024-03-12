f68976a9a5f72af72bcca1c5e9b67489ff12263f
DharmikPatel <dharmikpatel2200dr@gmail.com>
2024-03-12 09:52:21 +0530

    changes

--- index.js
+++ index.js
@@ -4,39 +4,24 @@ const io = new Server({
   cors: true
 })
 
-const emailToSocketmapping = new Map()
-const socketToEmailMapping = new Map()
 io.on('connection', socket => {
-  console.log('New Connection!')
-  socket.on('join-room', data => {
-    const { roomId, emailId } = data
-    console.log('a user connected', emailId, 'room', roomId)
-    emailToSocketmapping.set(emailId, socket.id)
-    socketToEmailMapping.set(socket.id, emailId)
-    socket.join(roomId)
-    socket.emit('joined-room', { roomId })
-    socket.broadcast.to(roomId).emit('user-connected', { emailId })
-  })
-  socket.on('call-user', data => {
-    const { emailId, offer } = data
-    const fromEmail = socketToEmailMapping.get(socket.id)
-    const socketId = emailToSocketmapping.get(emailId)
-    socket.to(socketId).emit('incoming-call', { offer, from: fromEmail })
-  })
-  socket.on('call-accepted', data => {
-    const { emailId, answer } = data
-    const fromEmail = socketToEmailMapping.get(socket.id)
-    const socketId = emailToSocketmapping.get(emailId)
-    socket.to(socketId).emit('call-accepted', { answer, from: fromEmail })
-  })
+  console.log('A user connected')
+
   socket.on('disconnect', () => {
-    console.log('user disconnected')
-    const emailId = socketToEmailMapping.get(socket.id)
-    if (emailId) {
-      socket.broadcast.emit('user-disconnected', { emailId })
-      emailToSocketmapping.delete(emailId)
-      socketToEmailMapping.delete(socket.id)
-    }
+    console.log('User disconnected')
+  })
+
+  // Signaling messages
+  socket.on('offer', offer => {
+    socket.broadcast.emit('offer', offer)
+  })
+
+  socket.on('answer', answer => {
+    socket.broadcast.emit('answer', answer)
+  })
+
+  socket.on('icecandidate', candidate => {
+    socket.broadcast.emit('icecandidate', candidate)
   })
 })
 
